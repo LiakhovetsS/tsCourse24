4ec4625922808b08f7435df66ffb637f4cbd9755
Stanislav <lyakhovetss@gmail.com>
2024-04-28 10:11:36 +0300

    task10

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,50 @@ const deerData: (string | number)[] = new Array<string | number>('Bambi', 1); //
 
 const testArray:readonly [number, string] = [10, 'test'];
 // testArray.push('test') 
+
+// interface IUser {
+//   name: string;
+// }
+
+// class User {
+//   public name!: string;
+// }
+
+// class Admin {
+//   public name!: string;
+//   public age!: number;
+// }
+
+// class Guest {}
+
+// const equal: User = new User();
+// const more: Admin = new Admin();
+// const less: Guest = new Guest();
+
+
+// const a: IUser = new User(); // Ok -> однакові поля
+// const b: IUser = new Admin(); // Ok -> у Admin полів більше
+// const c: IUser = new Guest(); // Error -> обов'язкові поля відсутні
+// const d: IUser = equal; // Ok -> однакові поля
+// const e: IUser = more; // Ok -> у Admin полів більше
+// const f: IUser = less; // Error -> обов'язкові поля відсутні
+
+// function foo(_p1: IUser): void {}
+
+// foo(new User()); // Ok -> однакові поля
+// foo(new Admin()); // Ok -> у Admin полів більше
+// foo(new Guest()); // Error -> обов'язкові поля відсутні
+
+// foo(equal); // Ok -> однакові поля
+// foo(more); // Ok -> у Admin полів більше
+// foo(less); // Error -> обов'язкові поля відсутні
+
+class User {
+  public name!: string;
+  public age!: number;
+}
+
+const user = new User();
+
+let type: typeof user; // type: { name: string; age: number; }
+let union: keyof typeof user; // union: 'name' | 'age'
